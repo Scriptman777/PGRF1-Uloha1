eb9991d1edc275668968fbfed67490425b9c9d04
Scriptman777 <dave56938@gmail.com>
2020-10-14 15:16:24 +0200

    Dashed Midpoit Madness

--- src/rasterize/DashedLineRasterizer.java
+++ src/rasterize/DashedLineRasterizer.java
@@ -6,26 +6,48 @@ public class DashedLineRasterizer extends LineRasterizer {
         super(rst);
     }
 
-
     public void line(int x1, int y1, int x2, int y2){
+       drawNormalLine(x1, y1, x2, y2);
+       makeItLookDashed(x1, y1, x2, y2);
+       /*
+       Vykreslit čáru přerušovanou čáru pomocí algoritmu Midpoint je prakticky nemožné, nicméně jsem se o to pokusil.
+       Čára nikdy nezačíná ani nekončí mezerou, způsob vykresolování je však VELMI neefektivní.
+        */
 
+
+    }
+
+    private void drawNormalLine(int x1, int y1, int x2, int y2) {
         int midx, midy;
-        midx=(x1+x2)/2;
-        midy=(y1+y2)/2;
+        midx = (x1 + x2) / 2;
+        midy = (y1 + y2) / 2;
 
-        raster.setPixel(midx,midy,0xffff00);
+        raster.setPixel(midx, midy, 0xffff00);
 
-        if ((Math.abs(x1-midx)>5) || (Math.abs(y1-midy)>5))
-        {
-            line(x1,y1,midx,midy);
+        if ((Math.abs(x1 - midx) > 1) || (Math.abs(y1 - midy) > 1)) {
+            drawNormalLine(x1, y1, midx, midy);
         }
 
-        if ((Math.abs(x2-midx)>5) || (Math.abs(y2-midy)>5))
-        {
-            line(midx,midy,x2,y2);
+        if ((Math.abs(x2 - midx) > 1) || (Math.abs(y2 - midy) > 1)) {
+            drawNormalLine(midx, midy, x2, y2);
         }
 
+    }
 
+    private void makeItLookDashed(int x1, int y1, int x2, int y2) {
+        int midx, midy;
+        midx = (x1 + x2) / 2;
+        midy = (y1 + y2) / 2;
+
+        raster.setPixel(midx, midy, 0x000000);
+
+        if ((Math.abs(x1 - midx) > 5) || (Math.abs(y1 - midy) > 5)) {
+            makeItLookDashed(x1, y1, midx, midy);
+        }
+
+        if ((Math.abs(x2 - midx) > 5) || (Math.abs(y2 - midy) > 5)) {
+            makeItLookDashed(midx, midy, x2, y2);
+        }
 
     }
 
